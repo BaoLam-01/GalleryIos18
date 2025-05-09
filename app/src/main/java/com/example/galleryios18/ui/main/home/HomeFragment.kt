8c734484d039f70dd56020953303b7865c6d5ddf
Lam_Dinh_Van <lam.dinhvan@tapbi.vn>
2025-05-09 18:12:13 +0700

    permission

--- app/src/main/java/com/example/galleryios18/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/galleryios18/ui/main/home/HomeFragment.kt
@@ -2,10 +2,8 @@ package com.example.galleryios18.ui.main.home
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
@@ -14,25 +12,47 @@ import com.example.galleryios18.R
 import com.example.galleryios18.databinding.FragmentHomeBinding
 import com.example.galleryios18.ui.base.BaseBindingFragment
 import com.example.galleryios18.ui.main.MainActivity
-import com.example.galleryios18.utils.PermissionUtils
 import com.example.galleryios18.utils.Utils
-import com.google.android.datatransport.cct.internal.LogEvent
 import com.tapbi.spark.launcherios18.utils.PermissionHelper
 import timber.log.Timber
 
 class HomeFragment : BaseBindingFragment<FragmentHomeBinding, HomeViewModel>() {
     private var requestPermission = true
-    private val notificationPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { _ ->
-        Timber.e("LamPro | notification - ")
-        Utils.startService(requireActivity(), true)
-    }
 
-    private val mediaPermissionLauncher = registerForActivityResult(
+    private val multiplePermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
-        Timber.tag("LamPro").e("LamPro | permission size: ${permissions.size} ")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            if (permissions[Manifest.permission.READ_MEDIA_IMAGES] == true || permissions[Manifest.permission.READ_MEDIA_VIDEO] == true) {
+                getAllMedia()
+            } else {
+                if (permissions[Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED] == true) {
+                    getAllMedia()
+                } else {
+                    // no media
+                }
+            }
+
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (permissions[Manifest.permission.READ_MEDIA_IMAGES] == true || permissions[Manifest.permission.READ_MEDIA_VIDEO] == true) {
+                getAllMedia()
+            } else {
+                // no media
+            }
+        } else {
+            if (permissions[Manifest.permission.READ_EXTERNAL_STORAGE] == true) {
+                getAllMedia()
+            } else {
+                // no media
+            }
+        }
+
+        if (permissions[Manifest.permission.POST_NOTIFICATIONS] == true) {
+            Utils.startService(requireActivity(), true)
+        } else {
+            Utils.startService(requireActivity(), true)
+        }
     }
 
     override fun getViewModel(): Class<HomeViewModel> {
@@ -76,9 +96,6 @@ class HomeFragment : BaseBindingFragment<FragmentHomeBinding, HomeViewModel>() {
         if (requestPermission) {
             requestPermission = false
             Timber.e("LamPro | onStart - requestPermission : $requestPermission")
-            if (!PermissionUtils.checkNotificationPermission(requireContext())) {
-                notificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-            }
 
             if (PermissionHelper.checkPermissionMedia(requireActivity())) {
                 val permissions =
@@ -86,20 +103,24 @@ class HomeFragment : BaseBindingFragment<FragmentHomeBinding, HomeViewModel>() {
                         arrayOf(
                             Manifest.permission.READ_MEDIA_VIDEO,
                             Manifest.permission.READ_MEDIA_IMAGES,
-                            Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+                            Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED,
+                            Manifest.permission.POST_NOTIFICATIONS
                         )
                     } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                         arrayOf(
                             Manifest.permission.READ_MEDIA_VIDEO,
-                            Manifest.permission.READ_MEDIA_IMAGES
+                            Manifest.permission.READ_MEDIA_IMAGES,
+                            Manifest.permission.POST_NOTIFICATIONS
+
                         )
                     } else {
                         arrayOf(
-                            Manifest.permission.READ_EXTERNAL_STORAGE
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.POST_NOTIFICATIONS
                         )
                     }
 
-                mediaPermissionLauncher.launch(permissions)
+                multiplePermissionLauncher.launch(permissions)
             } else {
                 getAllMedia()
             }
