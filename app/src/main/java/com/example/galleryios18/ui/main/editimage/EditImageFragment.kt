6024ea9ff56ce7b96786b0da9afdd446996a20fd
LAMPRO <lamdinh00689@gmail.com>
2025-05-13 23:15:06 +0700

    update rv adjust

--- app/src/main/java/com/example/galleryios18/ui/main/editimage/EditImageFragment.kt
+++ app/src/main/java/com/example/galleryios18/ui/main/editimage/EditImageFragment.kt
@@ -2,10 +2,12 @@ package com.example.galleryios18.ui.main.editimage
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearSnapHelper
 import com.example.galleryios18.R
 import com.example.galleryios18.databinding.FragmentEditImageBinding
 import com.example.galleryios18.ui.adapter.TypeEditAdapter
 import com.example.galleryios18.ui.base.BaseBindingFragment
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper
 
 class EditImageFragment : BaseBindingFragment<FragmentEditImageBinding, EditImageViewModel>() {
     private lateinit var typeEditAdapter: TypeEditAdapter
@@ -28,7 +30,14 @@ class EditImageFragment : BaseBindingFragment<FragmentEditImageBinding, EditImag
     }
 
     private fun initView() {
+        val snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(binding.layoutAdjust.rvTypeAdjust)
         binding.layoutAdjust.rvTypeAdjust.adapter = typeEditAdapter
+
+        OverScrollDecoratorHelper.setUpOverScroll(
+            binding.layoutAdjust.rvTypeAdjust,
+            OverScrollDecoratorHelper.ORIENTATION_HORIZONTAL
+        )
     }
 
     private fun listener() {
